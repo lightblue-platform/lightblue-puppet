c06c49b094667d66a5d19e3a2c21f2ace39987fc
Burak Serdar <bserdar@redhat.com>
2014-05-16 08:29:10 -0600

    lint

--- manifests/awsnode.pp
+++ manifests/awsnode.pp
@@ -4,21 +4,21 @@ class lightblue::awsnode {
   include lightblue::eap
 
   file { [ '/etc/redhat', '/etc/redhat/lightblue' ] :
-        ensure   => 'directory',
-        owner    => 'jboss',
-        group    => 'jboss',
-        mode     => '0755',
-        requires => [ User['jboss'] ]
+    ensure   => 'directory',
+    owner    => 'jboss',
+    group    => 'jboss',
+    mode     => '0755',
+    requires => [ User['jboss'] ]
   }
 
   class { 'lightblue::jcliff':
-     config_dir      => '/etc/redhat/lightblue',
-     management_host => 'localhost',
-     management_port => 9999,
-     log_dir         => '/usr/share/jbossas/standalone/log',
-     enable_logging  => true,
-     deploy_apps     => true,
-     requires        => [ File['/etc/redhat/lightblue'] ],
+    config_dir      => '/etc/redhat/lightblue',
+    management_host => 'localhost',
+    management_port => 9999,
+    log_dir         => '/usr/share/jbossas/standalone/log',
+    enable_logging  => true,
+    deploy_apps     => true,
+    requires        => [ File['/etc/redhat/lightblue'] ],
   }
 
 }
