c7b29d1ae9634ccf8d6e63a2448a72c8e4b91a0b
Naveen Malik <nmalik@redhat.com>
2014-07-18 17:14:49 -0400

    Testing out hiera parameterized class magic

--- manifests/jcliff.pp
+++ manifests/jcliff.pp
@@ -23,11 +23,10 @@ class lightblue::jcliff (
   $jboss_home='/usr/share/jbossas',
   $enable_logging=false,
   $log_dir=undef,
-  $deploy_apps=true
-  ) {
-
-    $package_name = hiera('lightblue::jcliff::package::name', 'jcliff')
-    $package_ensure = hiera('lightblue::jcliff::package::ensure', latest)
+  $deploy_apps=true,
+  $package_name,
+  $package_ensure
+) {
 
     package { $package_name :
         ensure  => $package_ensure,
