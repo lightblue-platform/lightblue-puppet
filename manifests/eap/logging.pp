1fce436fdba2c8c04488dfea148ec949a43e667a
Naveen Malik <nmalik@redhat.com>
2014-07-18 11:32:01 -0400

    Fixed some hiera keys

--- manifests/eap/logging.pp
+++ manifests/eap/logging.pp
@@ -1,8 +1,8 @@
 class lightblue::eap::logging {
     # setup eap6 logging
-    $logging_format = hiera('lightblue::jboss::logging::format', '%d [%t] %-5p [%c] %m%n')
-    $root_log_level = hiera('lightblue::jboss::logging::level::root', WARN)
-    $com_redhat_log_level = hiera('lightblue::jboss::logging::level::lightblue', $root_log_level)
+    $logging_format = hiera('lightblue::eap::logging::format', '%d [%t] %-5p [%c] %m%n')
+    $root_log_level = hiera('lightblue::eap::logging::level::root', WARN)
+    $com_redhat_log_level = hiera('lightblue::eap::logging::level::lightblue', $root_log_level)
 
     lightblue::jcliffconfig { 'logging.conf':
         content => template('lightblue/logging.conf.erb'),
--- manifests/eap/module.pp
+++ manifests/eap/module.pp
@@ -1,20 +1,20 @@
 class lightblue::eap::module {
     # get all hystrix config variables
-    $hystrix_command_default_execution_isolation_strategy = hiera('lightblue::hystrix::command::default::execution_isolation_strategy', 'THREAD')
-    $hystrix_command_default_execution_isolation_thread_timeoutInMilliseconds = hiera('lightblue::hystrix::command::default::execution_isolation_thread_timeoutInMilliseconds', 60000)
-    $hystrix_command_default_circuitBreaker_enabled = hiera('lightblue::hystrix::command::default::circuitBreaker_enabled', false)
-    $hystrix_command_mongodb_execution_isolation_timeoutInMilliseconds = hiera('lightblue::hystrix::command::mongodb::execution_isolation_timeoutInMilliseconds', 50000)
-    $hystrix_threadpool_mongodb_coreSize = hiera('lightblue::hystrix::threadpool::mongodb_coreSize', 30)
+    $hystrix_command_default_execution_isolation_strategy = hiera('lightblue::eap::module::hystrix::command::default::execution_isolation_strategy::default', 'THREAD')
+    $hystrix_command_default_execution_isolation_thread_timeoutInMilliseconds = hiera('lightblue::eap::module::hystrix::command::default::execution_isolation_thread_timeoutInMilliseconds', 60000)
+    $hystrix_command_default_circuitBreaker_enabled = hiera('lightblue::eap::module::hystrix::command::default::circuitBreaker_enabled', false)
+    $hystrix_command_mongodb_execution_isolation_timeoutInMilliseconds = hiera('lightblue::eap::module::hystrix::command::mongodb::execution_isolation_timeoutInMilliseconds', 50000)
+    $hystrix_threadpool_mongodb_coreSize = hiera('lightblue::eap::module::hystrix::threadpool::mongodb::coreSize', 30)
 
-    $mongo_server_host = hiera('lightblue::mongo::server_host', undef)
-    $mongo_server_port = hiera('lightblue::mongo::server_port', undef)
-    $mongo_servers_cfg = hiera('lightblue::mongo::servers', undef)
-    $mongo_ssl = hiera('lightblue::mongo::ssl', true)
-    $mongo_noCertValidation = hiera('lightblue::mongo::noCertValidation', false)
-    $mongo_auth_mechanism = hiera('lightblue::mongo::auth::mechanism')
-    $mongo_auth_username = hiera('lightblue::mongo::auth::username')
-    $mongo_auth_password = hiera('lightblue::mongo::auth::password')
-    $mongo_auth_source = hiera('lightblue::mongo::auth::source')
+    $mongo_server_host = hiera('lightblue::eap::module::datastore::mongo::server_host', undef)
+    $mongo_server_port = hiera('lightblue::eap::module::datastore::mongo::server_port', undef)
+    $mongo_servers_cfg = hiera('lightblue::eap::module::datastore::mongo::servers', undef)
+    $mongo_ssl = hiera('lightblue::eap::module::datastore::mongo::ssl', true)
+    $mongo_noCertValidation = hiera('lightblue::eap::module::datastore::mongo::noCertValidation', false)
+    $mongo_auth_mechanism = hiera('lightblue::eap::module::datastore::mongo::auth::mechanism')
+    $mongo_auth_username = hiera('lightblue::eap::module::datastore::mongo::auth::username')
+    $mongo_auth_password = hiera('lightblue::eap::module::datastore::mongo::auth::password')
+    $mongo_auth_source = hiera('lightblue::eap::module::datastore::mongo::auth::source')
 
     # Setup the properties directory
     file { [ '/usr/share/jbossas/modules/com',
