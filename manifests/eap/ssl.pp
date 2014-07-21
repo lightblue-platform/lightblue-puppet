fb01f5fd3495847cda7fe658eba9bfea7b9e4d71
Naveen Malik <nmalik@redhat.com>
2014-07-21 09:51:45 -0400

    Fixing puppetlint warnings

--- manifests/eap/ssl.pp
+++ manifests/eap/ssl.pp
@@ -42,7 +42,7 @@ class lightblue::eap::ssl (
 ) {
     if $enabled {
         # pull certificate from the source
-        file { "${certificate_file}":
+        file { $certificate_file:
             owner   => 'root',
             group   => 'root',
             mode    => '0600',
@@ -52,12 +52,12 @@ class lightblue::eap::ssl (
         #This will create the keystore at the target location, with the alias eap6 to the cert
         java_ks { "${keystore_alias}:keystore":
             ensure       => latest,
-            certificate  => "${certificate_file}",
-            private_key  => "${certificate_file}",
+            certificate  => $certificate_file,
+            private_key  => $certificate_file,
             target       => "${keystore_location}/eap6.keystore",
             password     => $keystore_password,
             trustcacerts => true,
-            require      => File["${certificate_file}"],
+            require      => File[$certificate_file],
         }
         java_ks { "${keystore_alias}:truststore":
             ensure       => latest,
@@ -65,7 +65,7 @@ class lightblue::eap::ssl (
             target       => "${keystore_location}/eap6trust.keystore",
             password     => $keystore_password,
             trustcacerts => true,
-            require      => File["${certificate_file}"],
+            require      => File[$certificate_file],
         }
         file {"${keystore_location}/eap6.keystore":
             owner   => 'jboss',
