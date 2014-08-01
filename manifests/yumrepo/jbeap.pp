a0dade991457381c55f0ab7f671287cc2b8e72e6
Chris Callegari <ccallega@redhat.com>
2014-08-01 15:16:02 -0400

    Pulling baseurl out of class parameter and using hiera() to establish value

--- manifests/yumrepo/jbeap.pp
+++ manifests/yumrepo/jbeap.pp
@@ -1,11 +1,13 @@
 class lightblue::yumrepo::jbeap (
-    $baseurl,
     $descr='JBEAP repo',
     $enabled=1,
     $gpgcheck=0,
     $gpgkey=absent,
     $metadata_expire=absent,
 ) {
+
+  $baseurl = hiera('lightblue::yumrepo::lightblue::baseurl', 'http://pulp-gca03.util.phx1.redhat.com/pulp/repos/re/JBEAP-610')
+
   yumrepo { 'JBEAP':
     baseurl         => $baseurl,
     descr           => $descr,
