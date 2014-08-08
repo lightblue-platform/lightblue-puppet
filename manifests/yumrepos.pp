6e5c3ef9fd14cc122cafd127dc32e195fdf13bf5
Chris Callegari <ccallega@redhat.com>
2014-08-08 16:15:31 -0400

    Removing files that don't exist in devaws branch

--- manifests/yumrepos.pp
+++
@@ -1,6 +0,0 @@
-define lightblue::yumrepos ( $repos=[] ) {
-  $repo_names=split(inline_template("<%= repos.collect { |n| n['name'] } . join(',') %>"),',')
-  arryumrepo{ $repo_names:
-    all_repos => $repos
-  }
-}
