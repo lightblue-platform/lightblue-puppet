fcce82c19587789ec48f7a4bffad98091930de07
Naveen Malik <nmalik@redhat.com>
2014-08-12 17:41:23 -0400

    Added missing paren

--- manifests/eap/logging.pp
+++ manifests/eap/logging.pp
@@ -2,6 +2,7 @@ class lightblue::eap::logging (
     $format = '%d [%t] %-5p [%c] %m%n',
     $root_log_level = WARN,
     $lightblue_log_level = WARN,
+)
 {
     # setup eap6 logging
     lightblue::jcliff::config { 'logging.conf':
