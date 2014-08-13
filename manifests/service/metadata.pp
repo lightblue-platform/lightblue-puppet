42dbe99dac882c9f27e2c36d1dad8b639a2c4dc0
Derek Haynes <dhaynes@redhat.com>
2014-08-13 12:06:40 -0400

    Remove whitespace in service classes

--- manifests/service/metadata.pp
+++ manifests/service/metadata.pp
@@ -25,7 +25,7 @@ class lightblue::service::metadata inherits lightblue::service {
         ensure  => $package_ensure,
         require => [ Class['lightblue::yumrepo::lightblue'], Class['lightblue::eap'] ],
     }
-    
+
     if $package_name == 'lightblue-rest-metadata-cert-auth' {
       include lightblue::authentication::certificate
     }
