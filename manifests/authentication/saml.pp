0bd30b2e1d071bc55bca3ee7b502fd821e2967df
Derek Haynes <dhaynes@redhat.com>
2014-08-26 16:53:43 -0400

    Parameterize saml system properties

--- manifests/authentication/saml.pp
+++ manifests/authentication/saml.pp
@@ -10,7 +10,16 @@
 # Module requires no global variables.
 #
 # Setup saml base authentication.
-class lightblue::authentication::saml {
+class lightblue::authentication::saml (
+    $identity_url,
+    $service_url,
+    $keystore_url,
+    $keystore_pass,
+    $signing_key_pass,
+    $signing_key_alias,
+    $validating_key_alias,
+    $validating_alias_value",
+) {
     include lightblue::eap
 
     lightblue::jcliff::config { 'lightblue-security-domain-saml.conf':
