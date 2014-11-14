6a68ad1158a146fddfabb594218bd787227c69bf
Alec Henninger <ahenning@redhat.com>
2014-11-13 20:01:02 -0500

    Add class for datamgmt

---
+++ manifests/application/datamgmt.pp
@@ -0,0 +1,33 @@
+# == Class: lightblue::application::datamgmt
+#
+# Deploys lightblue data management application.
+#
+# === Parameters
+#
+# No parameters.
+#
+# === Variables
+#
+# Module requires no global variables.
+#
+# === Example
+#
+# include lightblue::application::datamgmt
+#
+class lightblue::application::datamgmt (
+    $package_name = 'lightblue-data-mgmt',
+    $package_ensure = latest,
+)
+inherits lightblue::application {
+    include lightblue::logging
+    include lightblue::yumrepo::lightblue
+
+    package { $package_name :
+        ensure  => $package_ensure,
+        require => [ Class['lightblue::yumrepo::lightblue'], Class['lightblue::eap'] ],
+    }
+
+    if $package_name == 'lightblue-data-mgmt-saml-auth' {
+      include lightblue::authentication::saml
+    }
+}
