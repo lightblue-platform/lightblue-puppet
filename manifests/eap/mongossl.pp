9e1c3def4978b20dcd9130a3478cbf4af18abe3b
Naveen Malik <nmalik@redhat.com>
2014-12-02 11:43:07 -0500

    Updated lightblue::eap::mongossl to allow for certificate_source OR certificate_content for some flexibility

--- manifests/eap/mongossl.pp
+++ manifests/eap/mongossl.pp
@@ -1,15 +1,29 @@
 class lightblue::eap::mongossl (
-    $certificate_content,
     $certificate_file,
     $java_ks_password,
+    $certificate_content='',
+    $certificate_source='',
 ) {
-    file { $certificate_file:
-        ensure      => file,
-        content     => $certificate_content,
-        owner       => 'jboss',
-        group       => 'jboss',
-        mode        => '0600',
-        require     => Package[$lightblue::eap::package_name],
+
+    if ($certificate_source == '') {
+        # no source is set, assume content is set
+        file { $certificate_file:
+            ensure      => file,
+            owner       => 'jboss',
+            group       => 'jboss',
+            mode        => '0600',
+            require     => Package[$lightblue::eap::package_name],
+            content     => $certificate_content,
+        }
+    } else {
+        file { $certificate_file:
+            ensure      => file,
+            owner       => 'jboss',
+            group       => 'jboss',
+            mode        => '0600',
+            require     => Package[$lightblue::eap::package_name],
+            source      => $certificate_source,
+        }
     }
 
     java_ks { "mongossl:${lightblue::java::java_home}/jre/lib/security/cacerts":
