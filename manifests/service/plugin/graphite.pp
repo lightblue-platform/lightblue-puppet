a3eb13bbb1b5d2d55f0de52d8c4a7570a788742d
Dennis Crissman <dcrissma@redhat.com>
2014-12-09 11:13:32 -0500

    add graphite prefix envvar

--- manifests/service/plugin/graphite.pp
+++ manifests/service/plugin/graphite.pp
@@ -4,6 +4,8 @@
 #
 # === Parameters
 #
+# $prefix   - (optional) Prefix used for graphite.
+#             Note: If not specified, then lightblue will use other strategies to determine the prefix.
 # $hostname - Hostname of the graphite server.
 #             If not set, puppet will remove the environment variables.
 # $port     - Port the graphite server is listening on
@@ -16,7 +18,7 @@
 #
 # include lightblue::service::graphite
 #
-class lightblue::service::plugin::graphite ($hostname = undef, $port = undef){
+class lightblue::service::plugin::graphite ($prefix = undef, $hostname = undef, $port = undef){
 
   $graphite_file = '/etc/profile.d/graphite.sh'
 
@@ -25,12 +27,14 @@ class lightblue::service::plugin::graphite ($hostname = undef, $port = undef){
       fail('If providing a graphite hostname, a port must also be provided.')
     }
 
+    $warning = '#This file is managed by Puppet, any changes will be overridden.'
+
     file { $graphite_file:
       ensure  => 'file',
       owner   => 'root',
       group   => 'root',
       mode    => '0755',
-      content => inline_template("export GRAPHITE_HOSTNAME=${hostname}\nexport GRAPHITE_PORT=${port}")
+      content => inline_template("${warning}\n\n<% if(!@prefix.nil?) -%>export GRAPHITE_PREFIX=${prefix}\n<% end -%>export GRAPHITE_HOSTNAME=${hostname}\nexport GRAPHITE_PORT=${port}\n")
     }
   }
   else{
