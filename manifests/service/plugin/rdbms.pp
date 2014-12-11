a7a757918c1739296a20d847bf541928dece0a20
Derek Haynes <dhaynes@redhat.com>
2014-12-11 09:00:02 -0500

    Add rdbms classes/templates

---
+++ manifests/service/plugin/rdbms.pp
@@ -0,0 +1,23 @@
+# Installs an RDBMS data source
+class lightblue::service::plugin::rdbms (
+    $dbms = 'oracle',
+    $data_source_name = 'rdbms',
+    $jndi_name = 'java:/rdbms',
+    $connection_url = 'jdbc:oracle:oci@db',
+    $user_name = 'user',
+    $password = 'password',
+) {
+
+  if $dbms = 'oracle' {
+    $driver_name = 'oracle'
+
+    lightblue::jcliff::config { 'lightblue-rdbms-oracle-driver.conf':
+      content => template('lightblue/lightblue-rdbms-oracle-driver.conf.erb')
+    }
+  }
+
+  lightblue::jcliff::config { 'lightblue-rdbms-datasource.conf':
+    content => template('lightblue/lightblue-rdbms-datasource.conf.erb')
+  }
+
+}
