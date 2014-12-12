459619e0884ed48742add5f03d06050fcc43cb3c
Alec Henninger <ahenning@redhat.com>
2014-12-12 16:27:13 -0500

    Include eap::client inside apps

--- manifests/application/datamgmt.pp
+++ manifests/application/datamgmt.pp
@@ -17,6 +17,13 @@
 class lightblue::application::datamgmt (
     $package_name = 'lightblue-data-mgmt',
     $package_ensure = latest,
+    $data_uri,
+    $metadata_uri,
+    $use_cert_auth = false,
+    $auth_cert_source = undef,
+    $auth_cert_content = undef,
+    $auth_cert_password = undef,
+    $ssl_ca_source = undef,
 )
 inherits lightblue::application {
     include lightblue::logging
@@ -29,5 +36,14 @@ inherits lightblue::application {
 
     if $package_name == 'lightblue-data-mgmt-saml-auth' {
       include lightblue::authentication::saml
+    }
+
+    lightblue::eap::client { 'data-mgmt' 
+        data_uri           => ${data_uri},
+        metadata_uri       => ${metadata_uri},
+        use_cert_auth      => ${use_cert_auth},
+        auth_cert_source   => ${auth_cert_source},
+        auth_cert_password => ${auth_cert_password},
+        ssl_ca_source      => ${ssl_ca_source},
     }
 }
