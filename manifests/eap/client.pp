e83f271241720609c622fb163cb5d72526dd8721
Alec Henninger <ahenning@redhat.com>
2014-12-15 11:28:47 -0500

    Put back module directories (class does not depend on eap::module, just eap)

--- manifests/eap/client.pp
+++ manifests/eap/client.pp
@@ -72,7 +72,10 @@ define lightblue::eap::client (
 {
     $module_path = "/usr/share/jbossas/modules/com/redhat/lightblue/client/${name}/main"
 
-    $module_dirs = ['/usr/share/jbossas/modules/com/redhat/lightblue/client',
+    $module_dirs = ['/usr/share/jbossas/modules/com',
+        '/usr/share/jbossas/modules/com/redhat',
+        '/usr/share/jbossas/modules/com/redhat/lightblue',
+        '/usr/share/jbossas/modules/com/redhat/lightblue/client',
         "/usr/share/jbossas/modules/com/redhat/lightblue/client/${name}", 
         $module_path]
 
