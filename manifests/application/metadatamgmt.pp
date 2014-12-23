1b1016b97ca6fa43ab0e76e1cff076c8828c4be8
Alec Henninger <ahenning@redhat.com>
2014-12-23 13:23:12 -0500

    Revert changes to metadatamgmt.pp for now

--- manifests/application/metadatamgmt.pp
+++ manifests/application/metadatamgmt.pp
@@ -17,18 +17,8 @@
 class lightblue::application::metadatamgmt (
     $package_name = 'lightblue-metadata-mgmt',
     $package_ensure = latest,
-    $app_uri,
-    $data_service_uri,
-    $metadata_service_uri,
-    $use_cert_auth = false,
-    $auth_cert_source = undef,
-    $auth_cert_content = undef,
-    $auth_cert_password = undef,
-    $ssl_ca_source = undef,
 )
 inherits lightblue::application {
-    # Include base until application is updated to use new config; then just
-    # include lightblue::logging.
     include lightblue::base
     include lightblue::yumrepo::lightblue
 
@@ -38,19 +28,6 @@ inherits lightblue::application {
     }
 
     if $package_name == 'lightblue-metadata-mgmt-saml-auth' {
-        include lightblue::authentication::saml 
-
-        lightblue::jcliff::config { 'metadata-mgmt-system-properties.conf': 
-            content => "{ 'system-property' => { 'MetadataMgmtURL' => '${app_uri}' } }",
-        }
-    }
-
-    lightblue::eap::client { 'metadata-mgmt' 
-        data_service_uri     => ${data_service_uri},
-        metadata_service_uri => ${metadata_service_uri},
-        use_cert_auth        => ${use_cert_auth},
-        auth_cert_source     => ${auth_cert_source},
-        auth_cert_password   => ${auth_cert_password},
-        ssl_ca_source        => ${ssl_ca_source},
+      include lightblue::authentication::saml
     }
 }
