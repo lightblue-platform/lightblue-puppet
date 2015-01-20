ecaed256d101c644d3552df976f13db383aef10d
Naveen Malik <nmalik@redhat.com>
2015-01-20 11:42:48 -0500

    Generalizing the template to support simple metadata roles and arrays of roles for each metadata roleMap entry

--- spec/classes/lightblue/eap/module/metadata_spec.rb
+++ spec/classes/lightblue/eap/module/metadata_spec.rb
@@ -83,6 +83,57 @@ describe 'lightblue::eap::module::metadata' do
           .with_content(/"metadata.delete.entity": \["delete.entity"\]/)
       end
     end
+
+    describe 'roleMap multiple values' do
+      let :params do
+        {
+          :directory => '/tmp',
+          :metadata_roles => {
+              'metadata.find.dependencies' => ['find.dependencies','find.dependencies2'],
+              'metadata.find.roles' => ['find.roles','find.roles2'],
+              'metadata.find.entityNames' => ['find.entityNames','find.entityNames2'],
+              'metadata.find.entityVersions' => ['find.entityVersions','find.entityVersions2'],
+              'metadata.find.entityMetadata' => ['find.entityMetadata','find.entityMetadata2'],
+              'metadata.insert' => ['insert','insert2'],
+              'metadata.insert.schema' => ['insert.schema','insert.schema2'],
+              'metadata.update.entityInfo' => ['update.entityInfo','update.entityInfo2'],
+              'metadata.update.schemaStatus' => ['update.schemaStatus','update.schemaStatus2'],
+              'metadata.update.defaultVersion' => ['update.defaultVersion','update.defaultVersion2'],
+              'metadata.delete.entity' => ['delete.entity','delete.entity2']
+          }
+        }
+      end
+
+      it do
+        should contain_file("/tmp/lightblue-metadata.json") \
+          .with({
+              'ensure'  => 'file',
+              'owner'   => 'jboss',
+              'group'   => 'jboss',
+              'mode'    => '0644'
+            }
+          ) \
+          .with_content(/"documentation":/) \
+          .with_content(/"type":/) \
+          .without_content(/"hookConfigurationParsers":/) \
+          .with_content(/"roleMap":/) \
+          .without_content(/"backend_parsers":/) \
+          .without_content(/"property_parsers":/) \
+          .with_content(/"dataSource": "metadata",/) \
+          .with_content(/"collection": "metadata"/) \
+          .with_content(/"metadata.find.dependencies": \["find.dependencies",[ ]*"find.dependencies2"\]/) \
+          .with_content(/"metadata.find.roles": \["find.roles",[ ]*"find.roles2"\]"/) \
+          .with_content(/"metadata.find.entityNames": \["find.entityNames",[ ]*"find.entityNames2"\]"/) \
+          .with_content(/"metadata.find.entityVersions": \["find.entityVersions",[ ]*"find.entityVersions2"\]"/) \
+          .with_content(/"metadata.find.entityMetadata": \["find.entityMetadata",[ ]*"find.entityMetadata2"\]"/) \
+          .with_content(/"metadata.insert": \["insert",[ ]*"insert2"\]"/) \
+          .with_content(/"metadata.insert.schema": \["insert.schema",[ ]*"insert.schema2"\]"/) \
+          .with_content(/"metadata.update.entityInfo": \["update.entityInfo",[ ]*"update.entityInfo2"\]"/) \
+          .with_content(/"metadata.update.schemaStatus": \["update.schemaStatus",[ ]*"update.schemaStatus2"\]"/) \
+          .with_content(/"metadata.update.defaultVersion": \["update.defaultVersion",[ ]*"update.defaultVersion2"\]"/) \
+          .with_content(/"metadata.delete.entity": \["delete.entity",[ ]*"delete.entity2"\]"/)
+      end
+    end
   end
 
 end
