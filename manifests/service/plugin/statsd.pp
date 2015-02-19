44674c688d35638d8fbc253c42599dc770868e68
Dennis Crissman <dcrissma@redhat.com>
2015-02-19 15:53:43 -0500

    add statsd plugin (similar to graphite)

--- manifests/service/plugin/statsd.pp
+++ manifests/service/plugin/statsd.pp
@@ -4,12 +4,12 @@
 #
 # === Parameters
 #
-# $prefix   - (optional) Prefix used for graphite.
+# $prefix   - (optional) Prefix used for statsd.
 #             Note: If not specified, then lightblue will use other strategies to determine the prefix.
-# $hostname - Hostname of the graphite server.
+# $hostname - Hostname of the statsd server.
 #             If not set, puppet will remove the environment variables.
-# $port     - Port the graphite server is listening on
-#             Defaults to 2003
+# $port     - Port the statsd server is listening on
+#             Defaults to 8125
 #
 # === Variables
 #
@@ -17,25 +17,25 @@
 #
 # === Example
 #
-# include lightblue::service::plugin::graphite
+# include lightblue::service::plugin::statsd
 #
-class lightblue::service::plugin::graphite ($prefix = undef, $hostname = undef, $port = 2003){
+class lightblue::service::plugin::statsd ($prefix = undef, $hostname = undef, $port = 8125){
 
-  $graphite_file = '/etc/profile.d/graphite.sh'
+  $statsd_file = '/etc/profile.d/lb-statsd.sh'
 
   if($hostname){
     $warning = '#This file is managed by Puppet, any changes will be overridden.'
 
-    file { $graphite_file:
+    file { $statsd_file:
       ensure  => 'file',
       owner   => 'root',
       group   => 'root',
       mode    => '0755',
-      content => inline_template("${warning}\n\n<% if(!@prefix.nil?) -%>export GRAPHITE_PREFIX=${prefix}\n<% end -%>export GRAPHITE_HOSTNAME=${hostname}\nexport GRAPHITE_PORT=${port}\n")
+      content => inline_template("${warning}\n\n<% if(!@prefix.nil?) -%>export STATSD_PREFIX=${prefix}\n<% end -%>export STATSD_HOSTNAME=${hostname}\nexport STATSD_PORT=${port}\n")
     }
   }
   else{
-    file { $graphite_file:
+    file { $statsd_file:
       ensure => 'absent'
     }
   }
