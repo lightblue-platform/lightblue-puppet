3fc3f35c02ff5de0d984a6454806f49f7627a47d
Luan Cestari <luan.cestari@gmail.com>
2015-03-30 23:05:34 -0300

    First part of the first for java/javac issue using puppet

--- manifests/java.pp
+++ manifests/java.pp
@@ -2,6 +2,8 @@
 # that support the /etc/alternatives configuration abstraction.  This will also
 # provide the JAVA_HOME environment variable for Java based projects that only
 # support the environment style configuration.
+include stdlib
+
 class lightblue::java ($java_version = '1.7.0', $java_distribution = 'openjdk') {
 
   $java_jre_location = $::architecture ? {
@@ -16,36 +18,41 @@ class lightblue::java ($java_version = '1.7.0', $java_distribution = 'openjdk') 
 
   $java_home = "/etc/alternatives/java_sdk_${java_distribution}"
 
+  anchor { 'lightblue::java::begin:': }
+  ->
   package { 'java':
-    ensure => installed,
+    ensure => [installed,$java_version],
     name   => "java-${java_version}-${java_distribution}",
   }
-
+  ->
   package { 'java-devel':
-    ensure => installed,
+    ensure => [installed,$java_version],
     name   => "java-${java_version}-${java_distribution}-devel",
   }
-
+  ->
+  file { '/etc/profile.d/java-env.sh':
+    mode    => '0755',
+    content => inline_template('export JAVA_HOME=<%= java_home %>'),
+  }
+  ->
   exec { "configure java_sdk_${java_distribution}":
     command => "/usr/sbin/alternatives --set java_sdk_${java_distribution} '${java_sdk_location}'",
     require => [File['/etc/profile.d/java-env.sh'],Package['java-devel']],
+    unless => "test /etc/alternatives/java -ef ${}"
     unless  => "/usr/sbin/alternatives --display java_sdk_${java_distribution} | grep link | grep ${java_sdk_location}"
   }
-
+  ->
   exec { 'configure java':
     command => "/usr/sbin/alternatives --set java '${java_jre_location}'",
     require => [File['/etc/profile.d/java-env.sh'],Package['java']],
     unless  => "/usr/sbin/alternatives --display java | grep link | grep ${java_jre_location}"
   }
-
+  ->
   exec { 'configure javac':
     command => "/usr/sbin/alternatives --set javac '${java_sdk_location}/bin/javac'",
     require => [File['/etc/profile.d/java-env.sh'],Package['java-devel']],
     unless  => "/usr/sbin/alternatives --display javac | grep link | grep ${java_sdk_location}/bin/javac"
   }
-
-  file { '/etc/profile.d/java-env.sh':
-    mode    => '0755',
-    content => inline_template('export JAVA_HOME=<%= java_home %>'),
-  }
+  ->
+  anchor { 'lightblue::java::end': }
 }
