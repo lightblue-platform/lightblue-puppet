7dd1e704d51c21e82732aa6754f1e1d8c304384b
Luan Cestari <luan.cestari@gmail.com>
2015-03-31 09:16:04 -0300

    Enhanced the constraints to run the comamnds

--- manifests/java.pp
+++ manifests/java.pp
@@ -35,23 +35,32 @@ class lightblue::java ($java_version = '1.7.0', $java_distribution = 'openjdk') 
     content => inline_template('export JAVA_HOME=<%= java_home %>'),
   }
   ->
-  exec { "configure java_sdk_${java_distribution}":
-    command => "/usr/sbin/alternatives --set java_sdk_${java_distribution} '${java_sdk_location}'",
+  exec { "configure java_sdk_${java_version}":
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] ,
+    command => "/usr/sbin/alternatives --set java_sdk_${java_version} '${java_sdk_location}'",
     require => [File['/etc/profile.d/java-env.sh'],Package['java-devel']],
-    unless => "test /etc/alternatives/java -ef ${}"
-    unless  => "/usr/sbin/alternatives --display java_sdk_${java_distribution} | grep link | grep ${java_sdk_location}"
+    unless => "test /etc/alternatives/java_sdk_${java_version} -ef ${java_sdk_location} && /usr/sbin/alternatives --display java_sdk_${java_version} | grep link | grep ${java_sdk_location}",
   }
   ->
   exec { 'configure java':
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] ,
     command => "/usr/sbin/alternatives --set java '${java_jre_location}'",
     require => [File['/etc/profile.d/java-env.sh'],Package['java']],
-    unless  => "/usr/sbin/alternatives --display java | grep link | grep ${java_jre_location}"
+    unless => "test /etc/alternatives/java -ef ${java_jre_location} && /usr/sbin/alternatives --display java | grep link | grep ${java_jre_location}",
   }
   ->
   exec { 'configure javac':
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] ,
     command => "/usr/sbin/alternatives --set javac '${java_sdk_location}/bin/javac'",
     require => [File['/etc/profile.d/java-env.sh'],Package['java-devel']],
-    unless  => "/usr/sbin/alternatives --display javac | grep link | grep ${java_sdk_location}/bin/javac"
+    unless => "test /etc/alternatives/javac -ef ${java_sdk_location}/bin/javac && /usr/sbin/alternatives --display javac | grep link | grep ${java_sdk_location}/bin/javac",
+  }
+  ->
+  exec { "configure java_sdk_${java_distribution}":
+    path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] ,
+    command => "/usr/sbin/alternatives --set java_sdk_${java_distribution} '${java_sdk_location}'",
+    require => [File['/etc/profile.d/java-env.sh'],Package['java-devel']],
+    unless => "test /etc/alternatives/java_sdk_${java_distribution} -ef ${java_sdk_location} && /usr/sbin/alternatives --display java_sdk_${java_distribution} | grep link | grep ${java_sdk_location}",
   }
   ->
   anchor { 'lightblue::java::end': }
