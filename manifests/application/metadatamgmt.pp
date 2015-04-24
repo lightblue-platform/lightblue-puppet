5a88083166401326cd1720f3366724e6aa6b164e
Marek Paterczyk <mpatercz@redhat.com>
2015-04-24 11:50:30 -0400

    Using lightblue::eap::client resource to define lb client configurations for metadata-mgmt app.

--- manifests/application/metadatamgmt.pp
+++ manifests/application/metadatamgmt.pp
@@ -30,4 +30,17 @@ inherits lightblue::application {
     if $package_name == 'lightblue-metadata-mgmt-saml-auth' {
       include lightblue::authentication::saml
     }
+
+    # It would be prettier to rename all mgmt_app keys in hiera for lightblue::application::metadatamgmt class
+    lightblue::eap::client { "metadata-mgmt":
+        data_service_uri => hiera('lightblue::endpoint::service::data'),
+        metadata_service_uri => hiera('lightblue::endpoint::service::metadata'),
+        use_cert_auth => hiera('lightblue::eap::module::mgmt_app_use_cert_auth'),
+        auth_cert_source => hiera('lightblue::eap::module::client_cert_source'),
+        auth_cert_password => hiera('lightblue::eap::module::mgmt_app_cert_password'),
+        auth_cert_file_path => hiera('lightblue::eap::module::mgmt_app_cert_file_path'),
+        auth_cert_alias => hiera('lightblue::eap::module::mgmt_app_cert_alias'),
+        ssl_ca_source => hiera('lightblue::eap::module::mgmt_app_ca_file_path'),
+    }
+
 }
