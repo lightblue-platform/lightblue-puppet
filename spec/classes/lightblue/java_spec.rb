33e7d1ed585ffcbd96ee3056b513ac71ad19a58b
Dennis Crissman <dcrissma@redhat.com>
2015-04-28 12:58:04 -0400

    support installed

--- spec/classes/lightblue/java_spec.rb
+++ spec/classes/lightblue/java_spec.rb
@@ -79,13 +79,13 @@ describe 'lightblue::java' do
     it do
       should contain_package('java').with(
         {
-          :ensure =>'java-1.8.0-openjdk-1.8.0.40-24.b25.fc21.x86_64',
+          :ensure =>'1.8.0.40-24.b25.fc21.x86_64',
           :name => 'java-1.8.0-openjdk',
         }
       )
       should contain_package('java-devel').with(
         {
-          :ensure =>'java-1.8.0-openjdk-devel-1.8.0.40-24.b25.fc21.x86_64',
+          :ensure =>'1.8.0.40-24.b25.fc21.x86_64',
           :name => 'java-1.8.0-openjdk-devel',
         }
       )
@@ -148,6 +148,37 @@ describe 'lightblue::java' do
       )
     end
   end
+  
+  context 'rhel7 x64 installed' do
+    let :facts do
+      {
+        :osfamily => 'RedHat',
+        :architecture => 'x86_64',
+        :operatingsystemrelease => '7.0'
+      }
+    end
+    
+    let :params do
+      {
+        :java_specific_version => "installed"
+      }
+    end
+
+    it do
+      should contain_package('java').with(
+        {
+          :ensure => 'installed',
+          :name => 'java-1.7.0-openjdk',
+        }
+      )
+      should contain_package('java-devel').with(
+        {
+          :ensure => 'installed',
+          :name => 'java-1.7.0-openjdk-devel',
+        }
+      )
+    end
+  end
 
   context 'rhel7 x64 java specific minor version' do
     let :facts do
@@ -169,13 +200,13 @@ describe 'lightblue::java' do
     it do
       should contain_package('java').with(
         {
-          :ensure => 'java-1.8.0-openjdk-1.8.0.40-24.b25.fc21.x86_64',
+          :ensure => '1.8.0.40-24.b25.fc21.x86_64',
           :name => 'java-1.8.0-openjdk',
         }
       )
       should contain_package('java-devel').with(
         {
-          :ensure =>'java-1.8.0-openjdk-devel-1.8.0.40-24.b25.fc21.x86_64',
+          :ensure =>'1.8.0.40-24.b25.fc21.x86_64',
           :name => 'java-1.8.0-openjdk-devel',
         }
       )
