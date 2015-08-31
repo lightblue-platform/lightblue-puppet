13adae039f32d45ac5e3f0b4ae53114ff6fb8e1a
Naveen Malik <nmalik@redhat.com>
2015-08-31 16:23:00 -0400

    Cleanup to remove cacert class, split eap ssl into truststore class, referenced for mongossl
    Conflicts:
    	manifests/cacert.pp

--- manifests/eap/ssl.pp
+++ manifests/eap/ssl.pp
@@ -9,10 +9,10 @@
 #   alias to the keystore
 #
 # [*keystore_location*]
-#   Directory where keystore (and truststore) are saved.
+#   Directory where keystore is saved.
 #
 # [*keystore_password*]
-#   Password to keystore (and truststore).
+#   Password to keystore.
 #
 # [*server_certificate_source*]
 #   Location for server certificate.  Is used as 'source' in a 'file' entry.
@@ -20,13 +20,6 @@
 #
 # [*server_certificate_file*]
 #   Full path and filename for the server certificate.
-#
-# [*identity_certificate_source*]
-#   Location for identity certificate.  Is used as 'source' in a 'file' entry.
-#   Recommend referencing a file in a separate (and secure) puppet module for managing certs.
-#
-# [*identity_certificate_file*]
-#   Full path and filename for the identity certificate.
 #
 # === Variables
 #
@@ -38,24 +31,17 @@ class lightblue::eap::ssl (
     $keystore_password,
     $server_certificate_source,
     $server_certificate_file,
-    $identity_certificate_source,
-    $identity_certificate_file,
 )
 {
-    # pull certificates from the source
+    include lightblue::eap::truststore
+
+    # pull certificate from the source
     file { $server_certificate_file:
         owner  => 'root',
         group  => 'root',
         mode   => '0600',
         links  => 'follow',
         source => $server_certificate_source,
-    }
-    file { $identity_certificate_file:
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0600',
-        links  => 'follow',
-        source => $identity_certificate_source,
     }
     #This will create the keystore at the target location, with the alias eap6 to the cert
     java_ks { "${keystore_alias}:keystore":
@@ -67,25 +53,11 @@ class lightblue::eap::ssl (
         trustcacerts => true,
         require      => [ File[$server_certificate_file], Package[$lightblue::eap::package_name] ],
     }
-    java_ks { "${keystore_alias}:truststore":
-        ensure       => latest,
-        certificate  => $identity_certificate_file,
-        target       => "${keystore_location}/eap6trust.keystore",
-        password     => $keystore_password,
-        trustcacerts => true,
-        require      => [ File[$identity_certificate_file], Package[$lightblue::eap::package_name] ],
-    }
     file {"${keystore_location}/eap6.keystore":
         owner   => 'jboss',
         group   => 'jboss',
         mode    => '0600',
         require => Java_ks["${keystore_alias}:keystore"],
-    }
-    file {"${keystore_location}/eap6trust.keystore":
-        owner   => 'jboss',
-        group   => 'jboss',
-        mode    => '0600',
-        require => Java_ks["${keystore_alias}:truststore"],
     }
 
     # setup thread_pool (params loaded from hiera)
