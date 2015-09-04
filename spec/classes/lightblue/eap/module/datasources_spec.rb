1052a481884a2818c3066d371b113ad5fe89b2cc
Naveen Malik <nmalik@redhat.com>
2015-09-03 21:41:27 -0400

    Updated spec for new ldap datasource config

--- spec/classes/lightblue/eap/module/datasources_spec.rb
+++ spec/classes/lightblue/eap/module/datasources_spec.rb
@@ -31,6 +31,53 @@ describe 'lightblue::eap::module::datasources' do
 
     end
 
+    describe 'ldap' do
+      let :parms do
+        {
+          :directory => '/tmp',
+          :mongo_noCertValidation => true,
+          :mongo_auth_mechanism => 'MONGODB_CR_MECHANISM',
+          :mongo_auth_source => 'admin',
+          :mongo_auth_username => 'lightblue',
+          :mongo_auth_password => 'nothing',
+          :mongo_ssl => false,
+          :ldap_config => {
+              'datasourceName' => 'ldap',
+              'database' => 'db',
+              'bindabledn' => 'uid=admin,dc=example,dc=com',
+              'password' => 'passwd',
+              'numberOfInitialConnections' => '5',
+              'maxNumberOfConnections' => '10',
+              'servers' => {
+                'host' => 'localhost',
+                'port' => '456'
+              }
+          }
+        }
+      end
+
+      it do
+        should contain_file('/tmp/datasources.json') \
+          .with({
+              'ensure'  => 'file',
+              'owner'   => 'jboss',
+              'group'   => 'jboss',
+              'mode'    => '0644'
+            }
+          ) \
+          .with_content(/"ldap":/) \
+          .with_content(/"database": "db"/) \
+          .with_content(/"bindabledn": "uid=admin,dc=example,dc=com"/) \
+          .with_content(/"password": "passwd"/) \
+          .with_content(/"numberOfInitialConnections": 5/)\
+          .with_content(/"maxNumberOfConnections": 10/) \
+          .with_content(/"servers:"/) \
+          .with_content(/"host": "localhost"/) \
+          .with_content(/"port": "456"/)
+      end
+
+    end
+
   end
 
 end
