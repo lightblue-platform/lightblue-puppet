e28ea7f3fd17ec798f0da73142e77dfd14ba2a85
Naveen Malik <nmalik@redhat.com>
2015-09-04 10:56:21 -0400

    Fixed spec test for ldap datasource config, fixed bugs in erb found

--- spec/classes/lightblue/eap/module/datasources_spec.rb
+++ spec/classes/lightblue/eap/module/datasources_spec.rb
@@ -32,7 +32,8 @@ describe 'lightblue::eap::module::datasources' do
     end
 
     describe 'ldap' do
-      let :parms do
+
+      let :params do
         {
           :directory => '/tmp',
           :mongo_noCertValidation => true,
@@ -41,18 +42,18 @@ describe 'lightblue::eap::module::datasources' do
           :mongo_auth_username => 'lightblue',
           :mongo_auth_password => 'nothing',
           :mongo_ssl => false,
-          :ldap_config => {
+          :ldap_config => [{
               'datasourceName' => 'ldap',
               'database' => 'db',
               'bindabledn' => 'uid=admin,dc=example,dc=com',
               'password' => 'passwd',
               'numberOfInitialConnections' => '5',
               'maxNumberOfConnections' => '10',
-              'servers' => {
+              'servers' => [{
                 'host' => 'localhost',
                 'port' => '456'
-              }
-          }
+              }]
+          }]
         }
       end
 
@@ -71,7 +72,7 @@ describe 'lightblue::eap::module::datasources' do
           .with_content(/"password": "passwd"/) \
           .with_content(/"numberOfInitialConnections": 5/)\
           .with_content(/"maxNumberOfConnections": 10/) \
-          .with_content(/"servers:"/) \
+          .with_content(/"servers":/) \
           .with_content(/"host": "localhost"/) \
           .with_content(/"port": "456"/)
       end
