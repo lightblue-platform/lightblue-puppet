1f7b0599292067c9451164aec06e90729a3f57f3
Naveen Malik <nmalik@redhat.com>
2015-10-22 20:58:11 -0400

    Added lightblue::limits to allow setting hard and soft nproc values

---
+++ manifests/limits.pp
@@ -0,0 +1,26 @@
+# == Class: lightblue::limits
+#
+# Configures limits in /etc/security/limits.conf
+#
+# === Parameters
+#
+# [*config_dir*]
+#   The director in which jcliff configuration files are deployed.
+#
+# === Variables
+#
+# Module requires no global variables.
+#
+class lightblue::limits (
+    $soft_nproc = undef,
+    $hard_nproc = undef
+) {
+  file { '/etc/security/limits.d/10-nproc.conf':
+    ensure  => 'file',
+    content => template('lightblue/limits/nproc.conf.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    before  => Package[$package_name],
+  }
+}
