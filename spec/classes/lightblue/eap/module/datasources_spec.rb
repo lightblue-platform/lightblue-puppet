027ad42541b63c58ab7fd13c70baacea0280f247
Naveen Malik <nmalik@redhat.com>
2016-03-09 10:05:34 -0500

    Updated driverOptions for recent changes and additions to lightblue-mongo

--- spec/classes/lightblue/eap/module/datasources_spec.rb
+++ spec/classes/lightblue/eap/module/datasources_spec.rb
@@ -120,6 +120,35 @@ describe 'lightblue::eap::module::datasources' do
 
     end
 
+    describe 'data maxQueryTimeMS' do
+
+      let :params do
+        {
+          :directory => '/tmp',
+          :mongo_noCertValidation => true,
+          :mongo_auth_mechanism => 'MONGODB_CR_MECHANISM',
+          :mongo_auth_source => 'admin',
+          :mongo_auth_username => 'lightblue',
+          :mongo_auth_password => 'nothing',
+          :mongo_ssl => false,
+          :mongo_data_maxQueryTimeMS => 50000
+        }
+      end
+
+      it do
+        should contain_file('/tmp/datasources.json') \
+          .with({
+              'ensure'  => 'file',
+              'owner'   => 'jboss',
+              'group'   => 'jboss',
+              'mode'    => '0644'
+            }
+          ) \
+          .with_content(/"maxQueryTimeMS": "50000"/) 
+      end
+
+    end
+
     describe 'metadata and data writeConcern' do
 
       let :params do
