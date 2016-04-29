fbb11e3310ba8b65a763543789dad94d55b55ccb
Dennis Crissman <dcrissma@redhat.com>
2016-04-29 16:28:45 -0400

    add healthcheck

---
+++ manifests/application/healthcheck.pp
@@ -0,0 +1,38 @@
+# == Class: lightblue::application::migrator
+#
+# Deploys lightblue healthcheck application.
+#
+# === Parameters
+#
+class lightblue::application::migrator (
+    $package_name = 'lightblue-healthcheck',
+    $package_ensure = latest,
+    $data_service_uri,
+    $metadata_service_uri,
+    $use_cert_auth,
+    $cert_file_path,
+    $cert_password,
+    $cert_alias,
+    $client_ca_source,
+    $client_cert_source,
+) {
+  
+    package { $package_name :
+        ensure  => $package_ensure,
+        require => [
+            Class['lightblue::yumrepo::lightblue'],
+            Class['lightblue::eap'] ],
+    }
+    
+    lightblue::eap::client { 'test':
+        data_service_uri     => $data_service_uri,
+        metadata_service_uri => $metadata_service_uri,
+        use_cert_auth        => $use_cert_auth,
+        auth_cert_source     => $client_cert_source,
+        auth_cert_password   => $cert_password,
+        auth_cert_file_path  => $cert_file_path,
+        auth_cert_alias      => $cert_alias,
+        ssl_ca_source        => $client_ca_source
+    }
+  
+}
